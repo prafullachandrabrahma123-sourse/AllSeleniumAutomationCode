0389c49fbe1c5083e073ca027fbccb85c18851a4
Sam Smith <prafullachandrabrahma4251@gmail.com>
2026-02-03 17:48:42 +0530

    Latest

--- TestNGTutorial/src/CoreJavaTopics/PracticeJavaPrograms.java
+++ TestNGTutorial/src/CoreJavaTopics/PracticeJavaPrograms.java
@@ -7,22 +7,15 @@ public class PracticeJavaPrograms {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please Enter a number: ");
-		int num = sc.nextInt();
-		int original = num;
-	
-		int reversed =0;
+		System.out.println("Please Enter a String: ");
+		String num = sc.nextLine();
+		String reversed= " ";
 		
-		while(num > 0) {
-			int digit = num%10;
-			reversed = reversed*10+digit;
-			num = num/10;
+		for(int i= num.length()-1; i>=0; i--){
+			reversed = reversed + num.charAt(i);
 		}
-		if(original == reversed) {
-		System.out.println("Original number is Palindrome: " + original);
-		}else {
-			System.out.println("Original number is not a Palindrome: " + original);
-		}
+	
+		System.out.println("Reversed String is: "+reversed);
 
 	}
 
